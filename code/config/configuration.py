e76a3d64bdb799a51f7fcdcffc41c600ae2f94c0
Jose Luis Gomez Zurita <joseluis-master@hotmail.com>
2017-03-26 10:16:18 +0200

    Added SSD utils class load and priors

--- code/config/configuration.py
+++ code/config/configuration.py
@@ -2,7 +2,9 @@ import imp
 import time
 import os
 from distutils.dir_util import copy_tree
+from tools.ssd_utils import BBoxUtility
 import shutil
+import pickle
 
 
 class Configuration():
@@ -105,6 +107,9 @@ class Configuration():
             cf.valid_metrics = ['val_loss', 'val_avg_recall', 'val_avg_iou']
             cf.best_metric = 'val_avg_recall'
             cf.best_type = 'max'
+            if cf.model_name == 'ssd':
+                cf.dataset.priors = pickle.load(open('prior_boxes_ssd300.pkl', 'rb'))
+                cf.bbox_util = BBoxUtility(cf.dataset.n_classes, cf.dataset.priors)
         else:
             cf.train_metrics = ['loss', 'acc']
             cf.valid_metrics = ['val_loss', 'val_acc']
